1ad501b335c622423c8351d4df319e3bf024510e
Corey Farwell <coreyf@rwell.org>
2017-12-01 09:44:38 -0500

    Day 1, part 1.

---
+++ 2017/day-1/src/main.rs
@@ -0,0 +1,53 @@
+static INPUT: &str = include_str!("input");
+
+fn main() {
+    println!("solution: {}", captcha_solver(INPUT));
+}
+
+fn captcha_solver(captcha: &str) -> usize {
+    let captcha_bytes = captcha.as_bytes(); // assume string is ASCII
+
+    let mut sum = captcha_bytes
+        .windows(2)
+        .filter_map(|window| if window[0] == window[1] {
+            Some(ascii_byte_to_val(window[0]))
+        } else {
+            None
+        })
+        .sum();
+
+    if captcha_bytes.len() > 2 && captcha_bytes.first() == captcha_bytes.last() {
+        sum += ascii_byte_to_val(captcha_bytes[0]);
+    }
+
+    sum
+}
+
+/// b'4' → 4
+fn ascii_byte_to_val(byte: u8) -> usize {
+    (byte - b'0') as usize
+}
+
+#[test]
+fn provided_tests() {
+    assert_eq!(3, captcha_solver("1122"));
+    assert_eq!(4, captcha_solver("1111"));
+    assert_eq!(0, captcha_solver("1234"));
+    assert_eq!(9, captcha_solver("91212129"));
+}
+
+#[test]
+fn empty_test() {
+    assert_eq!(0, captcha_solver(""));
+}
+
+#[test]
+fn basic_tests() {
+    assert_eq!(1, captcha_solver("11"));
+    assert_eq!(0, captcha_solver("10"));
+}
+
+#[test]
+fn test_ascii_byte_to_val() {
+    assert_eq!(4, ascii_byte_to_val(b'4'))
+}
