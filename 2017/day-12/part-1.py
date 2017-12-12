63eee9cb0bd60127fc1c3ea319be37f1a59e9e0f
Corey Farwell <coreyf@rwell.org>
2017-12-12 00:18:22 -0500

    go to sleep part 1

---
+++ 2017/day-12/part-1.py
@@ -0,0 +1,18 @@
+import igraph
+
+with open('input') as file_:
+    input_ = file_.read()
+
+graph = igraph.Graph()
+
+input_lines = input_.strip().splitlines()
+for line in input_lines:
+    node1, _ = line.split(' <-> ')
+    graph.add_vertex(node1)
+
+for line in input_.strip().splitlines():
+    node1, rest = line.split(' <-> ')
+    for node2 in rest.split(', '):
+        graph.add_edge(node1, node2)
+
+print(len(graph.clusters()[0]))
