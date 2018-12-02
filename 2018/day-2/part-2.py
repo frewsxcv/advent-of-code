dbcd86760d7164aee2f726c7e73b46e6b93c7259
Corey Farwell <coreyf@rwell.org>
2018-12-02 08:02:35 -0800

    2-2

---
+++ 2018/day-2/part-2.py
@@ -0,0 +1,11 @@
+import collections
+import itertools
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+for box_id_1, box_id_2 in itertools.product(lines, repeat=2):
+    zipped = list(zip(box_id_1, box_id_2))
+    if sum(x != y for x, y in zipped) == 1:
+        print(''.join(x for x, y in zipped if x == y))
+        exit()
