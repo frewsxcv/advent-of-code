35add15c53d48bf87644c67bb214e08bba8525d7
Corey Farwell <coreyf@rwell.org>
2019-12-04 11:09:46 -0500

    2019 day 4 part 2

--- 2019/day-4/src/main.rs
+++ 2019/day-4/src/main.rs
@@ -1,34 +1,57 @@
 const INPUT_LOW: u64 = 245182;
 const INPUT_HIGH: u64 = 790572;
 
-fn has_adjacent_digits(n: u64) -> bool {
-    n.to_string().as_bytes().windows(2).any(|w| w[0] == w[1])
+mod part1 {
+    pub fn has_adjacent_digits(n: u64) -> bool {
+        n.to_string().as_bytes().windows(2).any(|w| w[0] == w[1])
+    }
+
+    pub fn does_not_decrease(n: u64) -> bool {
+        n.to_string().as_bytes().windows(2).all(|w| w[0] <= w[1])
+    }
 }
 
-fn does_not_decrease(n: u64) -> bool {
-    n.to_string().as_bytes().windows(2).all(|w| w[0] <= w[1])
+mod part2 {
+    pub fn has_adjacent_digits(n: u64) -> bool {
+        let mut s = n.to_string();
+        s.insert(0, '*');
+        s.push('*');
+        s.as_bytes().windows(4).any(|w| {
+            w[0] != w[1] && w[1] == w[2] && w[2] != w[3]
+        })
+    }
+
+    pub fn does_not_decrease(n: u64) -> bool {
+        n.to_string().as_bytes().windows(2).all(|w| w[0] <= w[1])
+    }
 }
 
 fn main() {
     let mut count = 0;
-
     for n in INPUT_LOW..INPUT_HIGH {
-        if has_adjacent_digits(n) && does_not_decrease(n) {
+        if part1::has_adjacent_digits(n) && part1::does_not_decrease(n) {
             count += 1;
         }
     }
+    println!("part 1: {}", count);
 
-    println!("part 1: {}", count)
+    count = 0;
+    for n in INPUT_LOW..INPUT_HIGH {
+        if part2::has_adjacent_digits(n) && part2::does_not_decrease(n) {
+            count += 1;
+        }
+    }
+    println!("part 2: {}", count);
 }
 
 #[cfg(test)]
 mod test {
-    use super::does_not_decrease;
+    use super::part1;
 
     #[test]
-    fn test_does_not_decrease() {
-        assert!(does_not_decrease(123456));
-        assert!(does_not_decrease(666666));
-        assert!(!does_not_decrease(666566));
+    fn test_part1_does_not_decrease() {
+        assert!(part1::does_not_decrease(123456));
+        assert!(part1::does_not_decrease(666666));
+        assert!(!part1::does_not_decrease(666566));
     }
 }
